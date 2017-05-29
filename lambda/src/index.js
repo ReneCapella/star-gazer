2890391d5e67d32d7e65ca8ab59061088c817555
Amanda Capella <mrs.acapella@gmail.com>
2017-05-29 12:44:57 -0700

    Hours are now read in non-military format

--- lambda/src/index.js
+++ lambda/src/index.js
@@ -17,7 +17,7 @@ var APP_ID = 'amzn1.ask.skill.92a24c78-c2ca-410f-aa76-9ea83c7dcf55';//Applicatio
 
 var SKILL_NAME = "Star Gazer";//Skill Name Goes here
 var WELCOME_MESSAGE = "I love to gaze at the stars. Tell me the zipcode from where you'll be stargazing tonight.";
-var GET_WEATHER_MESSAGE = "I'm learning how to tell you whether the weather is good for star gazing!";
+// var GET_WEATHER_MESSAGE = "I'm learning how to tell you whether the weather is good for star gazing!";
 var HELP_MESSAGE = "You can ask is the weather good for star gazing in a certain zipcode, or, you can say exit... What can I help you with?";
 var HELP_REPROMPT = "When would you like to see the stars?";
 var STOP_MESSAGE = "Happy gazing! Goodbye!";
@@ -58,14 +58,39 @@ var handlers = {
             var alexifyHours = function(){
                 for (var i = 0; i < clearHours.length; i++) {
                     var splitHour = clearHours[i].split(":");
-                    var justHour = splitHour[0] + " hundred hours";
-                    console.log("justHour clear " + justHour);
+                    var justHour = splitHour[0];
+                    if(justHour > 12){
+                        justHour = justHour - 12 + "pm";
+                    } else {
+                        if(justHour < 10 || justHour == 0){
+                            justHour = justHour.split("");
+                            justHour = justHour.splice(1, 1);
+                            if(justHour == 0){
+                                justHour = "midnight";
+                            } else {
+                                justHour = justHour + "am"
+                            };
+                        };
+                    };
                     alexaClearHours.push(justHour);
                 };
                 for (var i = 0; i < notClearHours.length; i++) {
                     var splitHour = notClearHours[i].split(":");
-                    var justHour = splitHour[0] + "hundred hours";
-                    console.log("justHour notclear " + justHour);
+                    var justHour = splitHour[0];
+
+                    if(justHour > 12){
+                        justHour = justHour - 12 + "pm";
+                    } else {
+                        if(justHour < 10 || justHour == 0){
+                            justHour = justHour.split("");
+                            justHour = justHour.splice(1, 1);
+                            if(justHour == 0){
+                                justHour = "midnight";
+                            } else {
+                                justHour = justHour + "am"
+                            };
+                        };
+                    };
                     alexaNotClearHours.push(justHour);
                 };
 
@@ -141,15 +166,15 @@ var handlers = {
                     alexifyHours();
                     console.log("alexa not clear " + alexaNotClearHours);
                     console.log("alexa clear " + alexaClearHours);
-                    console.log("clearHours " + clearHours);
-                    console.log("notClearHours " + notClearHours);
+                    // console.log("clearHours " + clearHours);
+                    // console.log("notClearHours " + notClearHours);
 
                     if(clearConditionsHourCount/ totalHoursNightCount == 1){
                         this.emit(':tell', "It's currently daytime in " + city + " but it will be clear all night tonight starting at sunset around " + sunset + "." + clearHours);
                     } else if (clearHours.length <= notClearHours.length){
                         this.emit(':tell', "It's currently daytime in " + city + " but I looked ahead at the forecast tonight. You will have the highest chance of star gazing at " + alexaClearHours);
                     } else if (clearHours.length > notClearHours.length){
-                        this.emit(':tell', "It's currently daytime in " + city + " but I looked ahead at the forecast. It will be mostly clear tonight. You're least like to see the stars at " + alexaNotClearHours);
+                        this.emit(':tell', "It's currently daytime in " + city + " but I looked ahead at the forecast. It will be mostly clear tonight. You're least likely to see the stars at " + alexaNotClearHours);
                     };
                 };
             });
@@ -263,122 +288,3 @@ var httpsGetForecast = function (myData, callback) {
     req.end();
 
 }
-
-
-
-
-
-
-///////////////////////////////////////////
-//=======================================//
-//============GRAVE======================//
-//=================YARD==================//
-//======in case zombies are needed=======//
-///////////////////////////////////////////
-
-// url: 'https://api.enphaseenergy.com/api/v2/systems/67/summary',
-// method: 'GET',
-// headers: {
-//   Accept: 'application/json',
-//   'Content-Type': 'application/json'
-// },
-// body: JSON.stringify({
-//   key: '5e01e16f7134519e70e02c80ef61b692',
-//   user_id: '4d7a45774e6a41320a'
-// })
-// }, function (error, response, body) {
-// if (!error && response.statusCode == 200) {
-//   console.log('BODY: ', body);
-//   var jsonResponse = JSON.parse(body); // turn response into JSON
-//
-//   // do stuff with the response and pass it to the callback...
-//
-//   callback(sessionAttributes,
-//       buildSpeechletResponse(intent.name, speechOutput, repromptText,
-//       shouldEndSession));
-// }
-// });
-
-// {
-//  "version": "1.0",
-//  "response": {
-//    "outputSpeech": {"type":"PlainText","text":"Text to speak back to the user."},
-//    "card": {
-//      "type": "Simple",
-//      "title": "Example of the Card Title",
-//      "content": "Example of card content. This card has just plain text content.\nThe content is formatted with line breaks to improve readability."
-//    }
-//  }
-// }
-//
-// {
-//  "version": "1.0",
-//  "response": {
-//    "outputSpeech": {"type":"PlainText","text":"Your Car-Fu car is on the way!"},
-//    "card": {
-//      "type": "Standard",
-//      "title": "Ordering a Car",
-//      "text": "Your ride is on the way to 123 Main Street!\nEstimated cost for this ride: $25",
-//      "image": {
-//        "smallImageUrl": "https://carfu.com/resources/card-images/race-car-small.png",
-//        "largeImageUrl": "https://carfu.com/resources/card-images/race-car-large.png"
-//      }
-//    }
-//  }
-// }
-// {
-//  "version": "1.0",
-//  "response": {
-//    "outputSpeech": {"type":"PlainText","text":"Please go to your Alexa app and link your account."},
-//    "card": {
-//      "type": "LinkAccount"
-//    }
-//  }
-// }
-
-
-// var states = {
-//     STARTMODE: '_STARTMODE'  // Prompt the user to start or restart the game.
-// };
-
-// var url = function(){
-//     return "http://en.wikipedia.org/w/api.php?action=query&format=json&list=search&utf8=1&srsearch=Albert+Einstein";
-// };
-// 'AMAZON.SearchAction<object@WeatherForecast>': function (req, res) {
-//     console.log(req);
-// },
-// 'AMAZON.SearchAction<object@WeatherForecast[weatherCondition]>': function (req, res) {
-//     console.log(req);
-// },
-// 'AMAZON.SearchAction<object@WeatherForecast[temperature]>': function (req, res) {
-//     console.log(req);
-// },
-// IF I HAD A DB
-// alexa.dynamoDBTableName = 'starGazing';
-
-// var getJSON = function (callback) {
-//     // HTTP - WIKPEDIA
-//     request.get(url(), function(error, response, body) {
-//         var d = JSON.parse(body);
-//         console.log(response);
-//         console.log(body);
-//         console.log("inside getJSON");
-//         var result = d.query.searchinfo.totalhits
-//         if (result > 0) {
-//             callback(result);
-//         } else {
-//             callback("ERROR")
-//         }
-//     });
-//
-//     // HTTPS
-//     // request.get(url2(), function(error, response, body) {
-//     //     var d = JSON.parse(body)
-//     //     var result = d.results
-//     //     if (result.length > 0) {
-//     //         callback(result[0].book_details[0].title)
-//     //     } else {
-//     //         callback("ERROR")
-//     //     }
-//     // })
-// };
